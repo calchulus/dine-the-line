926c112747b687b15106dbaae3446927f88f8df3
beckkramer <kramer.beck@gmail.com>
2017-04-07 14:50:20 -0500

    Adds initial PHP files and git settings.

---
+++ lib/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Set your OAuth credentials here
+// These credentials can be obtained from the 'Manage API Access' page in the
+// developers documentation (http://www.yelp.com/developers)
+$CONSUMER_KEY = NULL;
+$CONSUMER_SECRET = NULL;
+$TOKEN = NULL;
+$TOKEN_SECRET = NULL;
